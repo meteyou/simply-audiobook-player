97003f7cea385de3f2e1568717edf940dc4e7ac0
Stefan Dej <meteyou@gmail.com>
2023-11-06 23:16:19 +0100

    fix: fix filesize format in sizeof_fmt
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- WebActor.py
+++ WebActor.py
@@ -9,7 +9,7 @@ from bottle import Bottle, redirect, route, static_file, template, url
 def sizeof_fmt(num, suffix="B"):
     for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
         if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
+            return f"{num:3.1f} {unit}{suffix}"
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
 
